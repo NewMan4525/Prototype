8a74ce728ab442336f368677b7090c14fae52ec7
Andrey L <newman4525@gmail.com>
2022-05-09 00:50:59 +0300

    less_16_Is_Complite

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 'use strict';
-///
+
+const body = document.querySelector('body');
+body.style.backgroundColor = '#202124';
+
+class First {
+	constructor() {}
+	hello() {
+		console.log('Привет я метод родителя!');
+	}
+}
+
+
+const first = new First();
+class Second extends First {
+	constructor() {
+		super();
+	}
+	hello() {
+		super.hello();
+		console.log('А я наследуемый метод!');
+	}
+}
+
+const second = new Second();
+
+console.log(first);
+first.hello();
+console.log(second);
+second.hello();
